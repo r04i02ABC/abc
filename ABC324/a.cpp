82757fc6f157238cfd0a0c50d02999f033a67b4c
r04i02 <k10190202@gmail.com>
2023-10-14 21:38:24 +0900

    数字列すべて同じならYes

--- ABC324/a.cpp
+++ ABC324/a.cpp
@@ -1,8 +1,25 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
-#include <cmath> 
+#include <cmath>
 using namespace std;
 int main(void){
-    
+    int n;
+    int a[100];
+    int ans=0;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+
+    for(int i=0;i<n;i++){
+        if(a[1]==a[i]){
+            ans++;
+        }
+    }
+    if(ans==n){
+        cout << "Yes";
+    }else{
+        cout << "No";
+    }
 }
