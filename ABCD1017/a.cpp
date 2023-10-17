ed4c181ff66ada846aa057dd467f2dbc9cbe6380
r04i02 <k10190202@gmail.com>
2023-10-17 16:28:08 +0900

    Hello,World!ならAC違うならWA

---
+++ ABCD1017/a.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cmath> 
+using namespace std;
+int main(void){
+    string hw,s;
+    cin >> s;
+    hw = "Hello,World!";
+    if(hw==s){
+        cout << "AC";
+    }else{
+        cout << "WA";
+    }
+}
