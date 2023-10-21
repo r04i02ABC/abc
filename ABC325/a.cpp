61d71975411d23428142cd9a2e4211dd7eb43d18
r04i02 <k10190202@gmail.com>
2023-10-21 22:25:51 +0900

    苗字に" san"つけて出力

---
+++ ABC325/a.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cmath> 
+using namespace std;
+int main(void){
+    string s,t;
+    cin >> s >> t;
+    cout << s << " san";
+}
