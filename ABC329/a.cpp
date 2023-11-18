c25288f23cb9e4df516e162e27ab18b9adcd195d
r04i02 <k10190202@gmail.com>
2023-11-18 23:20:19 +0900

    1文字→空白→1文字を繰り返す

---
+++ ABC329/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int main(){
+    string s;
+    cin >> s;
+    int size = s.length();
+    rep(i, size){
+        cout << s[i] << " ";
+    }
+
+}
