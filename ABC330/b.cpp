fbe6548497ec2d17d2b7467d3e3fd3d899be31b5
r04i02 <k10190202@gmail.com>
2023-12-08 00:21:35 +0900

    下限以下は下限を、上限以上なら上限をその間なら自分自身の値を返す

---
+++ ABC330/b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int main(){
+    int n,l,r;
+    int a[200000];
+    cin >> n >> l >> r;
+    rep(i,n){
+        cin >> a[i];
+    }
+    rep(i,n){
+        if(a[i]>=r){
+            cout << r << " ";
+        }else if(a[i]<=l){
+            cout << l << " ";
+        }else{
+            cout << a[i] << " ";
+        }
+    }
+}
