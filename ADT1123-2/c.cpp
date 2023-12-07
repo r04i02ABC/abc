4ff0b709b76c8ef897f402d4bd1e606793b0d992
r04i02 <k10190202@gmail.com>
2023-12-08 00:32:02 +0900

    与えられた要素が2なら2つ目の要素の場所を解答、3なら2つ目の要素の場所を3つ目の要素に変更

--- ADT1123-2/c.cpp
+++ ADT1123-2/c.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using pint = pair<int,int>;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
