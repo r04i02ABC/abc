78dd99d189a6f88fb87dd9481d40afc34acd83cf
r04i02 <k10190202@gmail.com>
2023-12-24 00:00:17 +0900

    小さい方を出力

---
+++ ABC334/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+int main(){
+    int b,g;
+    cin >> b >> g;
+    if(b>g) cout << "Bat";
+    else cout << "Glove";
+}
