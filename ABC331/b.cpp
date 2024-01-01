7fdee183a3c98b06c6c1c27d0370c9afc46ba028
r04i02 <k10190202@gmail.com>
2024-01-01 23:43:50 +0900

    n個以上の卵を買うときにかかる最低金額を出力。6,8,12個入りを何個でも買ってよい

--- ABC331/b.cpp
+++ ABC331/b.cpp
@@ -7,41 +7,19 @@ using pint = pair<int,int>;
 #define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
 #define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
 #define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+int inf = 1410065408;
 int main(){
     int n,s,m,l;
     cin >> n >> s >> m >> l;
-    bool check = true;
-    bool check2 = true;
-    int ans=0;
-    int tmp;
-    n++;
-    if(l/12>m/8){
-        check=false;
-    }
-    if(m/8>s/6){
-        check2=false;
-    }
-    if(l/12>s/6){
-        check=false;
-    }
-    
-    if(check&&n>=24){
-        ans+=(n/12-2)*l;
-        if(l*2<m*3&&l*2<s*4){
-            ans=(n/12)*l;
-        }else if(m*3<s*4){
-            ans+=s*4;
-        }else{
-            ans+=m*3;
-        }    
-    }else if(check2&&n>=24){
-        if(m*3<s*4){
-            ans=(n/8-3)*m+s*4;
-        }else{
-            ans+=(n/8)*m;
+    int ans = inf;
+    rep(i,20){
+        rep(j,18){
+            rep(k,12){
+                if(i*6+j*8+k*12>=n){
+                    ans = min(ans,i*s+j*m+k*l);
+                }
+            }
         }
-    }else{
-        
     }
-    cout << ans;
+    cout << ans << endl;
 }
