66f1de0d1b175f8e298172ad5bde87f9455e547b
r04i02 <k10190202@gmail.com>
2024-01-02 22:26:37 +0900

    一票毎にそれまでの最大値と増えたものを比較して最大値かつ最小の番号の人を出力

--- ABC329/d.cpp
+++ ABC329/d.cpp
@@ -8,27 +8,22 @@ using pint = pair<int,int>;
 #define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
 #define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
 int main(){
-    ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
     int n,m;
-    int a[200000];
-    int b[200000] = {};
-    int max = 1;
-    int top = 0;
     cin >> n >> m;
+    vector<int> a(m);
+    vector<int> b(n + 1);
     rep(i,m){
         cin >> a[i];
     }
+
+    int top = 0;
     rep(i,m){
-        b[a[i]]+=1;
-        max = *std::max_element(b, b + 200000);
-        if(i==0){
-        rep1(j, n){
-            if(max==b[j]){
-                    cout << j << endl;
-                    break;
-                }
-            }
+        b[a[i]]++;
+        if(b[a[i]]>b[top]){
+            top = a[i];
+        }else if(b[a[i]]==b[top]){
+            top = min(a[i],top);
         }
+        cout << top << endl;
     }
 }
