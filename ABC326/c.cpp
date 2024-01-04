abbab81a585085ebd0c9e2c6727214c7a76c07ea
r04i02 <k10190202@gmail.com>
2024-01-04 23:49:49 +0900

    半開区間上のプレゼントの最大値を出力。配置されている場所が入力される

--- ABC326/c.cpp
+++ ABC326/c.cpp
@@ -1,51 +1,38 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <cstdio>
-#include <string>
-#include <cmath> 
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
 int main(void){
-    int n;
-    int m;
-    int tmp[300000];
-    vector<int> a;
-    vector<int> ans;
+    int n,m;
     cin >> n >> m;
-    bool check1 = true;
-    bool check2 = true;
-    int j=0;
-    int k=0;
-    int h=0;
-    for(int i=0;i<n;i++){
-        cin >> tmp[i];
-        a.push_back(tmp[i]);
+    vector<int> a(n);
+    int l=0;
+    int ans=0;
+    rep(i,n){
+        cin >> a[i];
     }
+
     sort(a.begin(), a.end());
-    while(check1){
-        k=j+1;
-        h=1;
-        while(check2){
-            if(j+k>=n){
-                check1 = false;
-                check2 = false;
-                if(a[k]-a[j]>=m){
-                    ans.push_back(h);
-                    k=0;
-                    check2=false;
-                }
+
+    rep(i,n){
+        if(a[i]-a[l]+1>m){
+            while(a[i]-a[l]>m){
+                l++;
             }
-            if(a[k]-a[j]>=m){
-                ans.push_back(h);
-                k=0;
-                check2=false;
-            }
-            k++;
-            h++;
-        } 
-        j++;
-        check2=true;
+            if(a[i]-a[l]==m) l++;
+            ans = max(ans, i-l+1);
+        }else{
+            ans = max(ans, i-l+1);
+        }
     }
-    int max = *max_element(ans.begin(), ans.end());
-    cout << max;
+    cout << ans;
+
 }
