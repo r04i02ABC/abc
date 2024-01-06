70e8130a411984b15b5bd04a6e387b4d0ca01264
r04i02 <k10190202@gmail.com>
2024-01-06 22:56:18 +0900

    3つの数の和がn以下になるように辞書順のような形で出力

---
+++ ABC335/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    int n; cin >> n;
+    vector<int> ans(3);
+    rep(i,n+1){
+        rep(j,n-i+1){
+            rep(k,n-i-j+1){
+                cout <<  i << " " << j << " " << k << endl;
+            }
+        }
+    } 
+}
