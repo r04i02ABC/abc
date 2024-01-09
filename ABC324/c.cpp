ec7e5a1a7f5ab60297fd15d84fc12f28349aefe0
r04i02 <k10190202@gmail.com>
2024-01-10 00:51:53 +0900

    全く同じ文字列または１文字違いまたは１文字消すか増やすかして同じになる物を出力

--- ABC324/c.cpp
+++ ABC324/c.cpp
@@ -1,28 +1,104 @@
-#include <cstdio>
-#include <iostream>
-#include <string>
-#include <cmath> 
+#include <bits/stdc++.h>
 using namespace std;
-int main(void){
-    int n;
+using ll = long long;
+using pint = pair<int,int>;
+using pll = pair<long long, long long>;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep1(i,n) for (int i = 1; i <= (int)(n); i++)
+#define YesNo(bool) if(bool) cout << "Yes" << endl; else cout << "No" << endl 
+#define YESNO(bool) if(bool) cout << "YES" << endl; else cout << "NO" << endl 
+bool compare(pint& p1, pint& p2){return p1.second < p2.second;}
+bool compare(pll& p1, pll& p2){return p1.second < p2.second;}//secondの値でfirstをsort
+//bool compare(all_of(check.begin(), check.end(), [](bool x){return x;}))//全ての要素がtrueならtrueを返す
+int main(){
+    int k;
     string t;
-    string s[50000];
-    cin >> n;
+    cin >> k;
     cin >> t;
-    for(int i=0;i<n;i++){
+    vector<string> s(k);
+    rep(i,k){
         cin >> s[i];
     }
 
-    for(int i=0;i<n;i++){
-        if(t==s[i]){
-            cout << i+1;
-            cout << 1 << endl;
-        }else if(s[i].compare(t)>0&&s[i].compare(t)==1){
-            cout << i+1;
-            cout << 2 << endl;
-        }else if(s[i].compare(t)<0&&s[i].compare(t)==-1){
-            cout << i+1;
-            cout << endl;
+    int Tsize = t.size();
+    int Ssize;
+    int ans = 0;
+    vector<int> ansList;
+
+    rep(i,k){
+        Ssize = s[i].size();
+        if(Ssize==Tsize&&s[i]==t){
+            ans++;
+            ansList.push_back(i);
+        }else if(Ssize==Tsize){
+            rep(j,Ssize){
+                if(s[i][j]!=t[j]){
+                    s[i][j] = t[j];
+                    if(s[i]==t){
+                        ans++;
+                        ansList.push_back(i);
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }else if(Ssize+1==Tsize){
+            if(t[0]+s[i]==t){
+                ans++;
+                ansList.push_back(i);
+                continue;
+            }
+            rep(j,Ssize){
+                if(s[i][j]!=t[j]){
+                    s[i].insert(s[i].begin()+j,t[j]);
+                    if(s[i]==t){
+                        ans++;
+                        ansList.push_back(i);
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+                if(j==Ssize-1){
+                    s[i].push_back(t[Tsize-1]);
+                    if(s[i]==t){
+                        ans++;
+                        ansList.push_back(i);
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }else if(Ssize==Tsize+1){
+            rep(j,Tsize){
+                if(s[i][j]!=t[j]){
+                    s[i].erase(s[i].begin()+j);
+                    if(s[i]==t){
+                        ans++;
+                        ansList.push_back(i);
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+                if(j==Tsize-1){
+                    s[i].pop_back();
+                    if(s[i]==t){
+                        ans++;
+                        ansList.push_back(i);
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+            }
         }
+    }
+    cout << ans << endl;
+    rep(i,ansList.size()){
+        cout << ansList[i]+1 << " ";
     }
 }
